4c92b337650945c5eea02fd03c75ee6447ea8af9
Jett Teixeira <jett.twalters@gmail.com>
2022-10-24 20:52:39 -0500

    feat: basic template

---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+(function() {
+	"use strict";
+	/**
+	 * Chore
+	 */
+	const select = (el, all = false) => {
+		el = el.trim();
+		if (all) {
+			return [...document.querySelectorAll(el)];
+		}
+		return document.querySelector(el);
+	}
+
+	const on = (type, el, listener, all = false) => {
+		let selectEl = select(el, all)
+		if (selectEl) {
+			if (all) {
+				selectEl.forEach(e => e.addEventListener(type, listener))
+			} else {
+				selectEl.addEventListener(type, listener)
+			}
+		}
+	}
+
+	const onscroll = (el, listener) => {
+		el.addEventListener('scroll', listener)
+	}
+
+	/**
+	 * Back-To-Top
+	 */
+	let backtotop = select('.back-to-top')
+	if (backtotop) {
+		const toggleBacktotop = () => {
+			if (window.scrollY > 100) {
+				backtotop.classList.add('active')
+			} else {
+				backtotop.classList.remove('active')
+			}
+		}
+		window.addEventListener('load', toggleBacktotop)
+		onscroll(document, toggleBacktotop)
+	}
+})();
