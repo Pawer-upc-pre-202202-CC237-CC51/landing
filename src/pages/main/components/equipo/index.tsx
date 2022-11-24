7a73629894613afeee5b5eb8c8d649de0bcb90c2
Jett Teixeira <JettTeixeira@users.noreply.github.com>
2022-11-24 07:22:31 -0500

    refactor: cambios en el estilo de la codificacion y linter (#31)

--- src/pages/main/components/equipo/index.tsx
+++ src/pages/main/components/equipo/index.tsx
@@ -1,4 +1,9 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import ImgTeam1 from "./team-1.jpg";
+import ImgTeam2 from "./team-2.jpg";
+import ImgTeam3 from "./team-3.jpg";
+import ImgTeam4 from "./team-4.jpg";
 import "./style.css";
 
 export type EquipoProps = {};
@@ -17,7 +22,7 @@ export const Equipo: React.FC<EquipoProps> = () => {
 					<div className="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
 						<div className="member">
 							<div className="member-img">
-								<img src="team-1.jpg" className="img-fluid" alt="">
+								<img src={ImgTeam1} className="img-fluid" alt="" />
 								<div className="social">
 									<a href="#team"><i className="bi bi-twitter"></i></a>
 									<a href="#team"><i className="bi bi-facebook"></i></a>
@@ -35,7 +40,7 @@ export const Equipo: React.FC<EquipoProps> = () => {
 					<div className="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
 						<div className="member">
 							<div className="member-img">
-								<img src="team-2.jpg" class="img-fluid" alt="">
+								<img src={ImgTeam2} className="img-fluid" alt="" />
 								<div className="social">
 									<a href=""><i className="bi bi-twitter"></i></a>
 									<a href=""><i className="bi bi-facebook"></i></a>
@@ -53,7 +58,7 @@ export const Equipo: React.FC<EquipoProps> = () => {
 					<div className="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="300">
 						<div className="member">
 							<div className="member-img">
-								<img src="team-3.jpg" class="img-fluid" alt="">
+								<img src={ImgTeam3} className="img-fluid" alt=""  />
 								<div className="social">
 									<a href=""><i className="bi bi-twitter"></i></a>
 									<a href=""><i className="bi bi-facebook"></i></a>
@@ -61,7 +66,7 @@ export const Equipo: React.FC<EquipoProps> = () => {
 									<a href=""><i className="bi bi-linkedin"></i></a>
 								</div>
 							</div>
-							<div class="member-info">
+							<div className="member-info">
 								<h4>Daniel Barrionuevo Gutiérrez</h4>
 								<span>Integrante</span>
 							</div>
@@ -71,7 +76,7 @@ export const Equipo: React.FC<EquipoProps> = () => {
 					<div className="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="400">
 						<div className="member">
 							<div className="member-img">
-								<img src="team-4.jpg" className="img-fluid" alt="">
+								<img src={ImgTeam4} className="img-fluid" alt="" />
 								<div className="social">
 									<a href=""><i className="bi bi-twitter"></i></a>
 									<a href=""><i className="bi bi-facebook"></i></a>
