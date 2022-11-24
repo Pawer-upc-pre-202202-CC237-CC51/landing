ad3392266c3dce7dfcc57649c6e001e859282fb9
Jett Teixeira <jett.twalters@gmail.com>
2022-11-24 01:17:41 -0500

    feat: se añadio la seccion de metricas

--- src/pages/main/components/metricas/index.tsx
+++ src/pages/main/components/metricas/index.tsx
@@ -4,9 +4,41 @@ import "./style.css";
 export type MetricasProps = {};
 
 export const Metricas: React.FC<MetricasProps> = () => {
-	return (
-		<div className="App">
 
-		</div>
+	React.useEffect(() => {
+		new (window as any).PureCounter();
+	}, []);
+
+	return (
+		<section id="metricas" className="metricas section-bg">
+			<div className="container">
+				<div className="row justify-content-end">
+					<div className="col-lg-3 col-md-5 col-6 d-md-flex align-items-md-stretch">
+						<div className="metricas-box">
+							<span data-purecounter-start="0" data-purecounter-end="30" data-purecounter-duration="2" className="purecounter"></span>
+							<p>Casas de adopción</p>
+						</div>
+					</div>
+					<div className="col-lg-3 col-md-5 col-6 d-md-flex align-items-md-stretch">
+						<div className="metricas-box">
+							<span data-purecounter-start="0" data-purecounter-end="400" data-purecounter-duration="2" className="purecounter"></span>
+							<p>Compañeros ideales</p>
+						</div>
+					</div>
+					<div className="col-lg-3 col-md-5 col-6 d-md-flex align-items-md-stretch">
+						<div className="metricas-box">
+							<span data-purecounter-start="0" data-purecounter-end="127" data-purecounter-duration="2" className="purecounter"></span>
+							<p>Animales adoptados</p>
+						</div>
+					</div>
+					<div className="col-lg-3 col-md-5 col-6 d-md-flex align-items-md-stretch">
+						<div className="metricas-box">
+							<span data-purecounter-start="0" data-purecounter-end="52" data-purecounter-duration="2" className="purecounter"></span>
+							<p>Animales encontrados</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
 	);
 };
