b991fcb715c654c66153d9e9b8d07fba70fd5c4f
Cesar Sanchez <102542350+CesarRafaelSanchez@users.noreply.github.com>
2022-11-24 10:22:12 -0500

    Feat/cta (#33)
    * feat: se agrego la seccion empieza tu busqueda en react
    
    * refacor: cambio codigo cta
    
    Co-authored-by: Jett Teixeira <jett.twalters@gmail.com>

--- src/pages/main/components/empiezaTuBusqueda/index.tsx
+++ src/pages/main/components/empiezaTuBusqueda/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import "./style.css";
 
@@ -5,8 +6,14 @@ export type EmpiezaTuBusquedaProps = {};
 
 export const EmpiezaTuBusqueda: React.FC<EmpiezaTuBusquedaProps> = () => {
 	return (
-		<div className="App">
-
-		</div>
+		<section id="cta" className="cta">
+			<div className="container" data-aos="zoom-in">
+				<div className="text-center">
+					<h3>Empieza tu búsqueda</h3>
+					<p> Empieza a buscar a tu compañero a través del catálogo de animales y casas de adopción que tenemos o ayuda en la búsqueda de animales perdidos.</p>
+					<a className="cta-btn" href="#">Empezar</a>
+				</div>
+			</div>
+		</section>
 	);
 };
